aa4ddd6e2579069a85b1c3ba05e72f3f6326d40a
Rahul Kore <rahulkore976@gmail.com>
2021-09-15 22:26:34 +0530

    bubble sort added

---
+++ Algorithms/Graphs/Dfs.java
@@ -0,0 +1,5 @@
+package Algorithms.Graphs;
+
+public class Dfs {
+    
+}
---
+++ Algorithms/sortingAlgorithms/BubbleSort.java
@@ -0,0 +1,28 @@
+package Algorithms.sortingAlgorithms;
+
+public class BubbleSort {
+
+    static void BubbleSort(int a[]){
+        int i,j,temp;
+        int exchange = 1;
+        for(i=0;i<a.length-1 && exchange ==1 ;i++){
+            exchange = 0;
+            for(j=0;j<a.length-1;j++){
+                if(a[j] > a[j+1]){
+                    exchange = 1;
+                    temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        int elements[]= {59,23,65,12,38,80,30,45,35};
+        BubbleSort(elements);
+        for (int i : elements) {
+            System.out.println(i);   
+        }
+    }
+}
