02d6839f4d0890422692e01cee25db1f973d3170
Rahul Kore <rahulkore976@gmail.com>
2021-09-16 13:47:22 +0530

    selection sort code added

--- Algorithms/sortingAlgorithms/SelectionSort.java
+++ Algorithms/sortingAlgorithms/SelectionSort.java
@@ -3,7 +3,21 @@ package Algorithms.sortingAlgorithms;
 public class SelectionSort {
 
     static void selectionSort(int a[]){
-        
+
+        for (int i = 0; i < a.length-1; i++)
+        {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i+1; j < a.length; j++)
+                if (a[j] < a[min_idx])
+                    min_idx = j;
+ 
+            // Swap the found minimum element with the first
+            // element
+            int temp = a[min_idx];
+            a[min_idx] = a[i];
+            a[i] = temp;
+        }
     }
 
     public static void main(String[] args) {
