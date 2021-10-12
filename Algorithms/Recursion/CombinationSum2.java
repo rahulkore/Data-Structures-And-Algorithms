5be6e03d564ba67b2701300d18c7bd56f358aa8c
Rahul Kore <rahulkore976@gmail.com>
2021-10-12 17:19:00 +0530

    questioomn

--- Algorithms/Recursion/CombinationSum2.java
+++ Algorithms/Recursion/CombinationSum2.java
@@ -1,5 +1,13 @@
 package Algorithms.Recursion;
 import java.util.*;
+
+/**
+ * Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+
+Each number in candidates may only be used once in the combination.
+
+Note: The solution set must not contain duplicate combinations.
+ */
 public class CombinationSum2 {
 
     /**
