072c2d7a236e4c0e0488807610b6eace401b0d6e
Rahul Kore <rahulkore976@gmail.com>
2021-10-14 21:02:00 +0530

    dijkstras algorithm added

---
+++ Algorithms/Graphs/DijkstrasAlgorithm.java
@@ -0,0 +1,48 @@
+package Algorithms.Graphs;
+import java.util.*;
+/**
+ * Dijkstras algorithm for shortest path
+ */
+public class DijkstrasAlgorithm {
+
+    private static void zeroOneBfs(int source,ArrayList<ArrayList<int[]>> adj, int[] dist ){
+        PriorityQueue<int[]> dq = new PriorityQueue<>();
+        dq.add(new int[]{source,0});
+        dist[source] = 0;
+        while(!dq.isEmpty()){
+            int[] data = dq.poll();
+            int u = data[0];
+            int du = data[1];
+            for(int[] it : adj.get(u)){
+                int v = it[0];
+                int wt = it[1];
+                if(du + wt < dist[v]){
+                    dist[v] = du + wt;
+                    dq.add(new int[]{v,du+wt});
+                }
+            }
+        }
+        for(int d : dist){
+            System.out.println(d);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<ArrayList<int[]>> list = new ArrayList<>();
+        int V = sc.nextInt();
+        int E = sc.nextInt();
+        for(int i = 0; i < V+1; i++)
+            list.add(i, new ArrayList<int[]>());
+        for(int i = 0; i < E; i++)
+        {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            int w = sc.nextInt();
+            list.get(u).add(new int[]{v,w});
+        }
+        int[] dist = new int[V];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        zeroOneBfs(0, list, dist);
+        sc.close();
+    }
+}
