59fa0b72e4c26be0fc38dd64031c449c6eae1beb
rahul <rahulkore976@gmail.com>
2021-11-03 18:19:21 +0530

    misc problems added

---
+++ misc/CountingBits.java
@@ -0,0 +1,41 @@
+package misc;
+/**
+ * Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: [0,1,1]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+Example 2:
+
+Input: n = 5
+Output: [0,1,1,2,1,2]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+3 --> 11
+4 --> 100
+5 --> 101
+ */
+public class CountingBits {
+
+    /**
+     * We can use brian kernighans algorithm over here and this will give us O(nlogn) solution 
+     */
+        public static int[] main(String[] args) {
+        int n= 5;
+        int ans[] = new int[n+1];
+        for(int i=0;i<=n;i++){
+            ans[i]=Integer.bitCount(i);
+        }
+        return ans;
+    }
+    
+}
---
+++ misc/ValidParenthesis.java
@@ -0,0 +1,22 @@
+package misc;
+import java.util.*;
+public class ValidParenthesis {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<Character>();
+        for (char it : s.toCharArray()) {
+            if (it == '(' || it == '[' || it == '{')
+                st.push(it);
+            else {
+                if(st.isEmpty()) return false;
+                char ch = st.pop(); 
+                if((it == ')' && ch == '(') ||  (it == ']' && ch == '[') || (it == '}' && ch == '{')) continue;
+                else return false;
+            }
+        }
+        return st.isEmpty();
+    }
+    public static void main(String[] args) {
+        
+    }
+    
+}
