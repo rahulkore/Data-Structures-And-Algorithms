86418aebfcd8356e19068c8461c459e0a99ea5c1
rahul <rahulkore976@gmail.com>
2021-11-05 19:00:55 +0530

    longest palindromic substring added

---
+++ misc/LongestPalindromicSubString.java
@@ -0,0 +1,45 @@
+package misc;
+
+public class LongestPalindromicSubString {
+
+    public static String main(String[] args) {
+        String s = "";
+        int ans = 0; // for counting the palindromic substring
+        int ansRow = 0; // maintaining the initial index for the longest palindromic substring
+        int ansCol = 0;// maintaining the final index for the longest palindromic substring
+        int longestLength = 0; // maintaining the length of the longest palindromic substring
+        boolean dp[][] = new boolean[s.length()][s.length()];
+        int gap; // for traversing diagonally and increasing the string length 
+        for(gap =0 ; gap < s.length() ; gap++){
+            for(int row = 0,col = gap; col < s.length(); row++,col++){
+                if(gap==0){
+                    dp[row][col] = true;                        
+                    ans+=1;
+                }
+                else if(gap == 1){
+                    dp[row][col] = s.charAt(row) == s.charAt(col);
+                    if(dp[row][col])
+                        ans+=1;
+                }
+                else{
+                    if(s.charAt(row) == s.charAt(col) && dp[row+1][col-1]==true){
+                        dp[row][col] = true;
+                        ans += 1;
+                    }else{
+                        dp[row][col] = false;
+                    }
+                }
+                if(dp[row][col]){
+                    if(col-row+1 > longestLength){
+                        longestLength = col-row+1;
+                        ansRow = row;
+                        ansCol = col;
+                    }
+                }
+            }
+        }
+        //System.out.println(ans);
+        return s.substring(ansRow, ansCol+1);
+    }
+    
+}
---
+++ misc/ReverseWordsInAString.java
@@ -0,0 +1,45 @@
+package misc;
+/**
+ * Given an input string s, reverse the order of the words.
+
+A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+
+Return a string of the words in reverse order concatenated by a single space.
+
+Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+ 
+
+Example 1:
+
+Input: s = "the sky is blue"
+Output: "blue is sky the"
+Example 2:
+
+Input: s = "  hello world  "
+Output: "world hello"
+Explanation: Your reversed string should not contain leading or trailing spaces.
+ */
+public class ReverseWordsInAString {
+
+    public static String main(String[] args) {
+        String s = "the sky is blue";
+        int n = s.length();
+        char[] charArray = s.toCharArray(); 
+        String ans = "";
+        for(int i=n-1;i>=0;i--){
+            if(charArray[i] != ' '){
+                String str = "";
+                while(charArray[i] != ' '){
+                    str += charArray[i];
+                    i--;
+                }
+                StringBuffer buf = new StringBuffer(str);
+                ans += buf.reverse().toString() + " ";
+
+            }
+        }
+        return ans.stripTrailing();
+    }
+    
+}
