3d7e10a9961af79da418d2b08be4cc1c974ab44d
rahul <rahulkore976@gmail.com>
2022-02-11 18:18:29 +0530

    fenwick tree added

---
+++ Algorithms/AdvanceDataStructure/FenwickTree.java
@@ -0,0 +1,49 @@
+package Algorithms.AdvanceDataStructure;
+
+public class FenwickTree {
+
+    private static int N;
+    int[] fen = new int[N];
+
+
+    /**Tc = O(logn) */
+    public void update(int i, int add){
+        while(i<N){
+            fen[i] += add;
+            /**To get the next index that will be affected 
+             * by the updation of value on i do the following steps
+             * 1 -> take 2's compliment of i 
+             * 2 -> perform & operation with the original i
+             * 3 -> add it with the original i  
+             * All three operations combined into 1 is given below*/ 
+            i += (i & (-i));
+        }
+    }
+
+
+    /**Tc = O(logn) */
+    public int sum(int i){
+        int s = 0;
+        while(i > 0){
+            s += fen[i];
+            /**To get the prev index to add it to the sum 
+             * perform the following operation on i 
+             * 1 -> take 2's compliment of i 
+             * 2 -> perform & operation with the original i
+             * 3 -> subtract it with the original i
+             * All three operations combined into 1 is given below
+             */
+            i = i - (i & (-i));
+        }
+        return s;
+    }
+
+    /**Tc = O(2*logn) */
+    public int range(int l, int r){
+        return sum(r) - sum(l-1);
+    }
+    public static void main(String[] args) {
+        
+    }
+    
+}
