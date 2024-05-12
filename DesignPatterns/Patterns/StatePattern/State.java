c5fdaa3c9d56c7d1eafd9dd817b63e8ba3d1803e
rahul kore <rahulkore976@gmail.com>
2024-05-12 19:34:20 +0530

    Adding State Pattern from head first design book

---
+++ DesignPatterns/Patterns/StatePattern/State.java
@@ -0,0 +1,15 @@
+package DesignPatterns.Patterns.StatePattern;
+
+public class State {
+
+    /**
+     * State Pattern 
+     * The state pattern allows an object to alter it's behavior when its internal state changes.
+     * The object will appear to change its class. 
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
