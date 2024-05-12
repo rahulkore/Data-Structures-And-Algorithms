0631b8cb5aaceb434fc7e75afd86493a43199b89
rahul kore <rahulkore976@gmail.com>
2024-05-12 18:20:01 +0530

    Adding Facade Pattern from Head First Design Book

---
+++ DesignPatterns/Patterns/FacadePattern/Facade.java
@@ -0,0 +1,15 @@
+package DesignPatterns.Patterns.FacadePattern;
+
+public class Facade {
+
+    /**
+     * Facade Pattern 
+     * The facade pattern provides a unified interface to a set of interfaces in a subsystem. 
+     * Facade defines a higher level interface that makes the subsystem easier to use. 
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
