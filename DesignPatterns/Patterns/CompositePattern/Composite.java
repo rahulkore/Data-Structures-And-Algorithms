ab88037b54de40c2a9e1f5fd947ab59cd8febc75
rahul kore <rahulkore976@gmail.com>
2024-05-12 18:59:48 +0530

    Adding Composite Pattern from Head First Design Book

---
+++ DesignPatterns/Patterns/CompositePattern/Composite.java
@@ -0,0 +1,16 @@
+package DesignPatterns.Patterns.CompositePattern;
+
+public class Composite {
+
+    /**
+     * Composite Pattern 
+     * The Composite pattern allows you to compose objects into tree structures to represent 
+     * part-whole hierarchies. Composite lets client treat individual objects & composition 
+     * of objects uniformly. 
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
