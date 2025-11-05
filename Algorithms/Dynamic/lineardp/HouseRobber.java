1aeef2728e6e6cc53c19fdcd7e3e2377cadb74d9
Rahul Kore <rahulkore976@gmail.com>
2025-11-04 23:18:20 -0800

    Create HouseRobber.java

---
+++ Algorithms/Dynamic/lineardp/HouseRobber.java
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/house-robber/description/ 
+
+
+class Solution {
+    public int houseRobber(int[] money) {
+        int n = money.length;
+        if(n == 1) return money[0];
+        int dp[] = new int[n];
+        Arrays.fill(dp, -1);
+        int max_without_first_house = helper(money,n-1,dp,1);
+        Arrays.fill(dp, -1);
+        int max_without_last_house = helper(money,n-2,dp,0);
+        return Math.max(max_without_first_house, max_without_last_house);
+    }
+    
+
+    private int helper(int[]nums, int ind, int[] dp, int end) {
+        if(ind == end) return nums[ind];
+        if(ind < end) return 0;
+        if(dp[ind] != -1) return dp[ind];
+        int rob_current_house = nums[ind] + helper(nums, ind-2, dp, end);
+        int not_rob_current_house = helper(nums, ind-1, dp, end);
+        return dp[ind] = Math.max(rob_current_house, not_rob_current_house);
+    }
+}
